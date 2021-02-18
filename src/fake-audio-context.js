d7684318eac4a4ec9557f458fb6607282abfd515
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2021-02-17 19:12:09 -0500

    Fixed bugs in FakeAudioContext

--- src/fake-audio-context.js
+++ src/fake-audio-context.js
@@ -1,12 +1,31 @@
 /* global jest */
 const { FakeMediaStream } = require('./fake-media-stream')
 
-class FakeAudioContext {
-  createMediaStreamSource () {
-    return {
-      connect: jest.fn(),
-      addTrack: jest.fn()
+class FakeAudioNode {
+  constructor (stream) {
+    this.tracks = new Set()
+    this.connect = jest.fn().mockImplementation(dst => {
+      [...this.tracks].forEach(t => {
+        if (dst.stream) {
+          dst.stream.addTrack(t)
+        } else {
+          dst.addTrack(t)
+        }
+      })
+      return dst
+    })
+    this.addTrack = jest.fn().mockImplementation(t => {
+      this.tracks.add(t)
+    })
+    if (stream) {
+      stream.getAudioTracks().forEach(this.addTrack, this)
     }
+  }
+}
+
+class FakeAudioContext extends FakeAudioNode {
+  createMediaStreamSource (stream) {
+    return new FakeAudioNode(stream)
   }
 
   createMediaStreamDestination () {
@@ -16,11 +35,11 @@ class FakeAudioContext {
   }
 
   createGain () {
-    return {
-      gain: {
-        value: 1.0
-      }
+    const node = new FakeAudioNode()
+    node.gain = {
+      value: 1.0
     }
+    return node
   }
 }
 
